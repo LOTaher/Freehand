f266b4d7eee45aca04dcd801576ae4dd6b67802d
Laith Taher <86690869+LOTaher@users.noreply.github.com>
2023-05-31 18:04:56 -0700

    added shadcn ui

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,14 @@
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
 
 export function Navbar() {
   const { data: session, status } = useSession();
@@ -44,24 +52,35 @@ export function Navbar() {
         </Link>
         {session ? (
           <>
-            <div className="px-4">
-              <Image
-                src={session.user.image as string}
-                width={40}
-                height={40}
-                alt="Profile Picture"
-                className="rounded-full"
-              />
-            </div>
-
-            <button
-              className="font-inter ml-4 rounded-md bg-[#6469ff] px-4 py-2 font-medium text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              onClick={() => {
-                signOut().catch(console.log);
-              }}
-            >
-              Sign Out
-            </button>
+            <DropdownMenu>
+              <DropdownMenuTrigger className="border-transparent focus:outline-none">
+                <div className="px-4 focus:outline-none">
+                  <Image
+                    src={session.user.image as string}
+                    width={40}
+                    height={40}
+                    alt="Profile Picture"
+                    className="transform rounded-full transition-transform duration-200 hover:scale-105"
+                  />
+                </div>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="mx-auto my-1 rounded-md border bg-white px-2 py-1">
+                <DropdownMenuItem className="font-inter rounded-md px-3 py-2 text-center font-semibold text-gray-700 transition-colors duration-200 hover:bg-gray-100 hover:text-gray-900 focus:outline-none">
+                  <Link href="/plan">Current Plan: Free</Link>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator className="my-1 border-gray-200" />
+                <DropdownMenuItem className="font-inter rounded-md bg-[#6469ff] px-3 py-2 text-center font-semibold text-white transition-colors duration-200 hover:bg-indigo-500 focus:outline-none">
+                  <button
+                    className="focus:outline-none"
+                    onClick={() => {
+                      signOut().catch(console.log);
+                    }}
+                  >
+                    Sign Out
+                  </button>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </>
         ) : (
           <>
