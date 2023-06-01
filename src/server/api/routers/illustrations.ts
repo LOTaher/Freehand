1341f799a923633a610104a35c35d6a851a63e03
Laith Taher <86690869+LOTaher@users.noreply.github.com>
2023-05-31 22:38:56 -0700

    trpc magic

--- src/server/api/routers/illustrations.ts
+++ src/server/api/routers/illustrations.ts
@@ -1,7 +1,27 @@
-import { createTRPCRouter, publicProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
+import { z } from "zod";
 
 export const illustrationRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.illustration.findMany();
   }),
+  create: protectedProcedure
+    .input(
+      z.object({
+        title: z.string().min(3).max(16),
+        src: z.string().url(),
+        link: z.string().url(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const illustration = await ctx.prisma.illustration.create({
+        data: {
+          title: input.title,
+          src: input.src,
+          link: input.link,
+        },
+      });
+
+      return illustration;
+    }),
 });
