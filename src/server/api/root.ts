770a786e9602f054cabcc3ebee5afff0d44d48bb
Laith Taher <86690869+LOTaher@users.noreply.github.com>
2023-06-01 11:01:49 -0700

    integrated stripe

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,7 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { illustrationRouter } from "~/server/api/routers/illustrations";
+import { stripeRouter } from "./routers/stripe";
+import { userRouter } from "./routers/user";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +10,8 @@ import { illustrationRouter } from "~/server/api/routers/illustrations";
  */
 export const appRouter = createTRPCRouter({
   illustrations: illustrationRouter,
+  stripe: stripeRouter,
+  user: userRouter,
 });
 
 // export type definition of API
---
+++ src/server/api/routers/user.ts
@@ -0,0 +1,30 @@
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+
+export const userRouter = createTRPCRouter({
+  subscriptionStatus: protectedProcedure.query(async ({ ctx }) => {
+    const { session, prisma } = ctx;
+
+    if (!session.user?.id) {
+      throw new Error("Not authenticated");
+    }
+
+    if (session.user?.role !== "USER") {
+      throw new Error("Not authenticated");
+    }
+
+    const data = await prisma.user.findUnique({
+      where: {
+        id: session.user?.id,
+      },
+      select: {
+        stripeSubscriptionStatus: true,
+      },
+    });
+
+    if (!data) {
+      throw new Error("Could not find user");
+    }
+
+    return data.stripeSubscriptionStatus;
+  }),
+});
