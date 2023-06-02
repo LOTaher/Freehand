932badf3db48446ed8351bc394f5646c2fd86dc9
Laith Taher <86690869+LOTaher@users.noreply.github.com>
2023-06-02 14:03:56 -0700

    added subscriptions successfully!

--- src/pages/api/stripe.ts
+++ src/pages/api/stripe.ts
@@ -1,9 +1,10 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { env } from "~/env.mjs";
+import type { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
+import { env } from "~/env.mjs";
 import { buffer } from "micro";
+import { prisma } from "~/server/db";
 
-const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
+const stripe = new Stripe(`${env.STRIPE_SECRET_KEY}`, {
   apiVersion: "2022-11-15",
 });
 
@@ -14,6 +15,7 @@ export const config = {
 };
 
 const webhook = async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log("here");
   if (req.method === "POST") {
     const buf = await buffer(req);
     const sig = req.headers["stripe-signature"] as string;
@@ -24,40 +26,41 @@ const webhook = async (req: NextApiRequest, res: NextApiResponse) => {
       event = stripe.webhooks.constructEvent(
         buf,
         sig,
-        env.STRIPE_WEBHOOK_SECRET
+        `${env.STRIPE_WEBHOOK_SECRET}`
       );
     } catch (err) {
       let message = "Unknown Error";
       if (err instanceof Error) message = err.message;
       res.status(400).send(`Webhook Error: ${message}`);
+      return;
     }
 
-    // Handle the event
     switch (event.type) {
-      case "subscription.payment_succeeded":
-        const checkoutSessionCompleted = event.data.object as {
+      case "checkout.session.completed":
+        const completedEvent = event.data.object as {
           id: string;
           metadata: {
             userId: string;
           };
         };
-        // Then define and call a function to handle the event checkout.session.completed
+
+        await prisma.user.update({
+          where: {
+            id: completedEvent.metadata.userId,
+          },
+          data: {
+            subscription: "PRO",
+          },
+        });
         break;
-      case "subscription.payment_failed":
-        const subscriptionScheduleCanceled = event.data.object as {
-          id: string;
-          metadata: {
-            userId: string;
-          };
-        };
-        // Then define and call a function to handle the event subscription_schedule.canceled
-        break;
+
+      // handle other event types: subscription payment, subscription cancellation, etc.
 
       default:
         console.log(`Unhandled event type ${event.type}`);
     }
 
-    res.json({ recieved: true });
+    res.json({ received: true });
   } else {
     res.setHeader("Allow", "POST");
     res.status(405).end("Method Not Allowed");
--- src/server/api/routers/checkout.ts
+++ src/server/api/routers/checkout.ts
@@ -1,20 +1,20 @@
-import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
-import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { env } from "~/env.mjs";
 import Stripe from "stripe";
 
-const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
+const stripe = new Stripe(`${env.STRIPE_SECRET_KEY}`, {
   apiVersion: "2022-11-15",
 });
 
 export const checkoutRouter = createTRPCRouter({
   createCheckout: protectedProcedure.mutation(async ({ ctx }) => {
     return stripe.checkout.sessions.create({
+      mode: "subscription",
       payment_method_types: ["card"],
       metadata: {
         userId: ctx.session.user.id,
       },
-      line_items: [{ price: env.STRIPE_PRICE_ID }],
+      line_items: [{ price: env.STRIPE_PRICE_ID, quantity: 1 }],
       success_url: `${env.HOST_NAME}`,
       cancel_url: `${env.HOST_NAME}`,
     });
