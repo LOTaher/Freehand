e394ed30aeb4c8faa906886d7b36e2da232c691b
Laith Taher <86690869+LOTaher@users.noreply.github.com>
2023-06-03 20:29:10 -0700

    config changes

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ await import("./src/env.mjs");
 const config = {
   reactStrictMode: true,
   images: {
-    domains: ["avatars.githubusercontent.com", "bit.ly"],
+    domains: ["avatars.githubusercontent.com", "bit.ly", "cdn.discordapp.com"],
   },
 
   /**
