c633714eea9c10909a2f6ff1c861c641fd6cd319
Laith Taher <86690869+LOTaher@users.noreply.github.com>
2023-06-12 16:01:24 -0700

    stripe payment hook

--- src/hooks/useBuySubscription.tsx
+++ src/hooks/useBuySubscription.tsx
@@ -2,9 +2,9 @@ import { loadStripe } from "@stripe/stripe-js";
 import { env } from "~/env.mjs";
 import { api } from "~/utils/api";
 
-const stripePromise = loadStripe(env.NEXT_PUBLIC_STRIPE_KEY);
+const stripePromise = loadStripe(`${env.NEXT_PUBLIC_STRIPE_KEY}`);
 
-export function buySubscription() {
+export function useBuySubscription() {
   const checkout = api.checkout.createCheckout.useMutation();
 
   return {
