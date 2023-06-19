f357f8547e6061d089f3b691acd98f6e0140d4d2
Laith Taher <86690869+LOTaher@users.noreply.github.com>
2023-06-18 22:08:21 -0700

    browse animations

--- src/pages/browse.tsx
+++ src/pages/browse.tsx
@@ -3,47 +3,52 @@ import { api } from "~/utils/api";
 import { BrowseNav } from "../components/BrowseNav";
 import Illustration from "~/components/Illustration";
 import type { NextPage } from "next";
+import { motion } from "framer-motion";
+import { Footer } from "~/components/Footer";
 // import { getSession } from "next-auth/react";
 // import { useSession } from "next-auth/react";
-
-{
-  /*
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export async function getServerSideProps(context: any) {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-  const session = await getSession(context);
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
-    };
-  }
-
-  return {
-    props: { session },
-  };
-}
-*/
-}
 
 const Browse: NextPage = () => {
   const { data: illustrations, isLoading } =
     api.illustrations.getAll.useQuery();
   // const { data: session } = useSession();
+  const FADE_DOWN_ANIMATION_VARIANT = {
+    hidden: { opacity: 0, y: -10 },
+    show: { opacity: 1, y: 0, transition: { type: "spring" } },
+  };
+
   return (
     <>
       <BrowseNav />
-      <div className="mx-auto max-w-7xl px-7 py-6">
-        <h1 className="text-[40px] font-extrabold text-[#222328]">
-          Illustrations
-        </h1>
-        <p className="mt-2 max-w-[500px] text-[20px] text-gray-500">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
-      </div>
+      <motion.div
+        initial="hidden"
+        animate="show"
+        viewport={{ once: true }}
+        variants={{
+          hidden: {},
+          show: {
+            transition: {
+              staggerChildren: 0.15,
+            },
+          },
+        }}
+      >
+        <div className="mx-auto max-w-7xl px-7 py-6">
+          <motion.h1
+            variants={FADE_DOWN_ANIMATION_VARIANT}
+            className="text-[40px] font-extrabold text-[#222328]"
+          >
+            Illustrations
+          </motion.h1>
+          <motion.p
+            variants={FADE_DOWN_ANIMATION_VARIANT}
+            className="mt-2 max-w-[500px] text-[20px] text-gray-500"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </motion.p>
+        </div>
+      </motion.div>
       <div className="mx-auto max-w-2xl px-4 py-4 sm:px-6 sm:py-3 lg:max-w-7xl lg:px-7">
         {isLoading && (
           <div className="flex items-center justify-center" role="status">
@@ -81,6 +86,7 @@ const Browse: NextPage = () => {
           .
         </p>
       </div>
+      <Footer />
     </>
   );
 };
