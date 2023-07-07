8e699d1cc2dd0623f9c3e998eedb22c035918aa7
Laith Taher <86690869+LOTaher@users.noreply.github.com>
2023-07-07 00:44:24 -0700

    button -> link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -98,14 +98,12 @@ function HomeNav() {
         >
           Browse
         </Link>
-        <button
-          onClick={() => {
-            void router.push("/pricing");
-          }}
+        <Link
+          href="/pricing"
           className="font-inter rounded-md px-4 py-2 font-medium text-gray-700 transition-colors duration-300 hover:bg-gray-100 hover:text-gray-900"
         >
           Pricing
-        </button>
+        </Link>
         {status === "loading" && (
           <div>
             <svg
